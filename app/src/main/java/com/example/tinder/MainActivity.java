81e8c9bf5130611f34ca623fb27dddfe2572b27f
Hoai Phong <duonghoaiphong65@gmail.com>
2019-04-06 08:44:31 +0700

    add home_fragment, login_fragment

--- app/src/main/java/com/example/tinder/MainActivity.java
+++ app/src/main/java/com/example/tinder/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.tinder;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+                        implements  HomeFragment.OnFragmentInteractionListener, LoginFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
