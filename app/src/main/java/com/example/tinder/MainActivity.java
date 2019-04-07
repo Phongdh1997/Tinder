76739977291a5da610c22c69873c12f7a88394d2
Hoai Phong <duonghoaiphong65@gmail.com>
2019-04-07 22:34:40 +0700

    hhh

--- app/src/main/java/com/example/tinder/MainActivity.java
+++ app/src/main/java/com/example/tinder/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_sign_up);
+        setContentView(R.layout.activity_main);
     }
 
     @Override
