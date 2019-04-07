96e21c36a217f238b5e55545748980ea57ce6631
Quoc Duong <1510599@hcmut.edu.vn>
2019-04-07 21:42:13 +0700

    signup

--- app/src/main/java/com/example/tinder/MainActivity.java
+++ app/src/main/java/com/example/tinder/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_login);
+        setContentView(R.layout.fragment_sign_up);
     }
 
     @Override
