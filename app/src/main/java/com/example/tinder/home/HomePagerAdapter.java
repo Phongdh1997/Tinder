267c085d60da477d652fd54cda3280558e8d62bc
Hoai Phong <duonghoaiphong65@gmail.com>
2019-04-15 13:57:53 +0700

    Revert "Merge pull request #13 from Phongdh1997/fix-home-state"
    This reverts commit 5f1006d8c9ceb1cde77d73b7adbf561fedb16891, reversing
    changes made to 57e8577924975a068380faf04f8ecedff7850503.

--- app/src/main/java/com/example/tinder/home/HomePagerAdapter.java
+++ app/src/main/java/com/example/tinder/home/HomePagerAdapter.java
@@ -1,120 +1,34 @@
 package com.example.tinder.home;
 
-import android.content.Context;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.view.PagerAdapter;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageButton;
-
-import com.example.tinder.R;
-
-import java.util.ArrayList;
-
-import androidx.navigation.Navigation;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 
 
-public class HomePagerAdapter extends PagerAdapter {
+public class HomePagerAdapter extends FragmentStatePagerAdapter {
 
-    private Context context;
-    private int[] resList;
+    private Fragment [] listFragment;
 
-    public HomePagerAdapter(Context context, int[] resList) {
-        this.context = context;
-        this.resList = resList;
+    public HomePagerAdapter(FragmentManager fm, Fragment [] listFragment) {
+        super(fm);
+        this.listFragment = listFragment;
     }
 
-    @NonNull
     @Override
-    public Object instantiateItem(@NonNull ViewGroup container, int position) {
-        View view = getView(container, resList[position]);
-        container.addView(view);
-        return view;
-    }
-
-    private View getView(ViewGroup container, int res) {
-        LayoutInflater inflater = LayoutInflater.from(this.context);
-        View retView = inflater.inflate(res, container, false);
-        switch (res) {
-            case R.layout.layout_tab_profile:
-                setUpControlForProfile(retView);
-                break;
-            case R.layout.layout_tab_search_friend:
-                break;
-            case R.layout.layout_tab_message_box:
-                setUpControlForMessageBox(retView);
-                break;
-                default:
-                    break;
-        }
-        return retView;
-    }
-
-    /**
-     *  set up controls for profile tab
-     */
-    private void setUpControlForProfile(View view) {
-        ImageButton btnToUserInfo = view.findViewById(R.id.btnToUserInfo);
-        btnToUserInfo.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_userInforFragment, null));
-    }
-
-    /**
-     *  set up controls for message box tab
-     */
-    private void setUpControlForMessageBox(View view) {
-        RecyclerView rvMatchList;
-        RecyclerView rvMessageList;
-        rvMatchList = view.findViewById(R.id.rvMatchList);
-        rvMessageList = view.findViewById(R.id.rvMessageList);
-        rvMatchList.setHasFixedSize(true);
-        rvMessageList.setHasFixedSize(true);
-
-        // use a linear layout manager
-        LinearLayoutManager layoutManager = new LinearLayoutManager(context);
-        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        rvMatchList.setLayoutManager(layoutManager);
-
-        layoutManager = new LinearLayoutManager(context);
-        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        rvMessageList.setLayoutManager(layoutManager);
-        rvMessageList.addItemDecoration(new DividerItemDecoration(context, DividerItemDecoration.VERTICAL));
-
-        ArrayList<String> list = new ArrayList<String>();
-        list.add("item 1");
-        list.add("item 2");
-        list.add("item 1");
-        list.add("item 2");
-        MatchListAdapter adapter = new MatchListAdapter(list);
-        rvMatchList.setAdapter(adapter);
-
-        MessageListAdapter mssAdapter = new MessageListAdapter(list);
-        rvMessageList.setAdapter(mssAdapter);
+    public Fragment getItem(int i) {
+        return this.listFragment[i];
     }
 
     @Override
     public int getCount() {
-        return resList.length;
-    }
-
-    @Override
-    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
-        return view == o;
+        return listFragment != null ? listFragment.length : 0;
     }
 
     @Nullable
     @Override
     public CharSequence getPageTitle(int position) {
         return null;
-    }
-
-    @Override
-    public void destroyItem(@NonNull ViewGroup collection, int position, @NonNull Object view) {
-        collection.removeView((View) view);
     }
 
 }
--- app/src/main/java/com/example/tinder/home/MessageBoxFragment.java
+++ app/src/main/java/com/example/tinder/home/MessageBoxFragment.java
@@ -5,28 +5,28 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TableLayout;
+import android.widget.SearchView;
 
 import com.example.tinder.R;
-import com.example.tinder.login.LoginFragment;
 
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link HomeFragment.OnFragmentInteractionListener} interface
+ * {@link MessageBoxFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link HomeFragment#newInstance} factory method to
+ * Use the {@link MessageBoxFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class MessageBoxFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -38,12 +38,10 @@ public class HomeFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    private View fragment;
+    private RecyclerView rvMatchList;
+    private RecyclerView rvMessageList;
 
-    private HomePagerAdapter homePagerAdapter;
-    private ViewPager pgHome;
-
-    public HomeFragment() {
+    public MessageBoxFragment() {
         // Required empty public constructor
     }
 
@@ -53,11 +51,11 @@ public class HomeFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
+     * @return A new instance of fragment MessageBoxFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
+    public static MessageBoxFragment newInstance(String param1, String param2) {
+        MessageBoxFragment fragment = new MessageBoxFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -75,11 +73,51 @@ public class HomeFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        this.fragment = inflater.inflate(R.layout.fragment_home, container, false);
-        return this.fragment;
+        return inflater.inflate(R.layout.fragment_message_box, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        addControls(view);
+        addEvents(view);
+    }
+
+    private void addEvents(View view) {
+
+    }
+
+    private void addControls(View view) {
+        rvMatchList = view.findViewById(R.id.rvMatchList);
+        rvMessageList = view.findViewById(R.id.rvMessageList);
+
+        rvMatchList.setHasFixedSize(true);
+        rvMessageList.setHasFixedSize(true);
+
+        // use a linear layout manager
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        rvMatchList.setLayoutManager(layoutManager);
+
+        layoutManager = new LinearLayoutManager(getContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        rvMessageList.setLayoutManager(layoutManager);
+        rvMessageList.addItemDecoration(new DividerItemDecoration(view.getContext(), DividerItemDecoration.VERTICAL));
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("item 1");
+        list.add("item 2");
+        list.add("item 1");
+        list.add("item 2");
+        MatchListAdapter adapter = new MatchListAdapter(list);
+        rvMatchList.setAdapter(adapter);
+
+        MessageListAdapter mssAdapter = new MessageListAdapter(list);
+        rvMessageList.setAdapter(mssAdapter);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -87,35 +125,6 @@ public class HomeFragment extends Fragment {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        Fragment [] listFragment = new Fragment[3];
-        listFragment[0] = new ProfileFragment();
-        listFragment[1] = new SearchFriendFragment();
-        listFragment[2] = new MessageBoxFragment();
-
-        homePagerAdapter = new HomePagerAdapter(getFragmentManager(), listFragment);
-        pgHome = view.findViewById(R.id.pgHome);
-        pgHome.setAdapter(homePagerAdapter);
-
-        TabLayout tabLayout = this.fragment.findViewById(R.id.tab_layout);
-        tabLayout.setupWithViewPager(pgHome);
-        tabLayout.setSelectedTabIndicatorHeight(0);
-
-        // add custome tab item
-        int[] imageResId = {
-                R.drawable.ic_person_gray_state,
-                R.drawable.ic_search_friend_state,
-                R.drawable.ic_message_state };
-
-        for (int i = 0; i < imageResId.length; i++) {
-            tabLayout.getTabAt(i).setIcon(imageResId[i]);
-        }
-
     }
 
     @Override
