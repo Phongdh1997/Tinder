dbcba050f9142be0993d9697bd3d381b85e0b078
ACV <acv@ACVs-MacBook-Pro.local>
2019-04-25 23:12:49 +0700

    revert master code

--- app/src/main/java/com/example/tinder/search_friend/SearchFriendPagerAdapter.java
+++ app/src/main/java/com/example/tinder/search_friend/SearchFriendPagerAdapter.java
@@ -1,8 +1,6 @@
 package com.example.tinder.search_friend;
 
-import android.arch.core.util.Function;
 import android.content.Context;
-import android.content.res.Resources;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -14,18 +12,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import org.json.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import org.json.JSONException;
-import java.io.Reader;
-import java.net.URL;
 
 import com.example.tinder.R;
 
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -37,87 +26,13 @@ public class SearchFriendPagerAdapter extends PagerAdapter {
     private ArrayList<String> dataBuff;
     private boolean isLoading;
 
-//    public static class MyJsonReader {
-//        public static String readAll(Reader rd) throws IOException {
-//            StringBuilder sb = new StringBuilder();
-//            int cp;
-//            while ((cp = rd.read()) != -1) {
-//                sb.append((char) cp);
-//            }
-//            return sb.toString();
-//        }
-//        /**
-//         * Hàm trả về JSONObject
-//         * @param url - Truyền link URL có định dạng JSON
-//         * @return - Trả về JSONOBject
-//         * @throws IOException
-//         * @throws JSONException
-//         */
-//        public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-//            InputStream is = new URL(url).openStream();
-//            try {
-//                //đọc nội dung với Unicode:
-//                BufferedReader rd = new BufferedReader
-//                        (new InputStreamReader(is, Charset.forName("UTF-8")));
-//                String jsonText = readAll(rd);
-//                JSONObject json = new JSONObject(jsonText);
-//                return json;
-//            } finally {
-//                is.close();
-//            }
-//        }
-//    }
-
-    private static String readText(Context context, int resId) throws IOException {
-        InputStream is = context.getResources().openRawResource(resId);
-        BufferedReader br= new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb= new StringBuilder();
-        String s= null;
-        while((  s = br.readLine())!=null) {
-            sb.append(s);
-            sb.append("\n");
-        }
-        return sb.toString();
-    }
-
     public SearchFriendPagerAdapter(Context context) {
         this.context = context;
         this.isLoading = false;
         dataBuff = new ArrayList<>();
-
-        dataBuff = new ArrayList<>();
-        for (int i = 1; i < 5; i++) {
+        for (int i = 1; i < 30; i++) {
             dataBuff.add("item" + i);
         }
-
-//        String url = "https://jsoneditoronline.org/?id=f65ef4edea3b4feda2aba75b58ae12a2";
-//        JSONObject jsonObject = new JSONObject();
-//        JSONObject userObj = new JSONObject();
-//        try {
-//            //đọc và chuyển về JSONObject
-//            jsonObject = MyJsonReader.readJsonFromUrl(url);
-//        } catch (IOException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        } catch (JSONException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//        for (int i = 0; i < jsonObject.length(); i++) {
-//            try {
-//                userObj = jsonObject.getJSONObject("user"+i);
-//            } catch(JSONException e) {
-//                e.printStackTrace();
-//            }
-//            if (userObj.has("user"+i)){
-//                try {
-//                    dataBuff.add(userObj.getJSONObject("user"+i).getString("name"));
-//                } catch(JSONException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-
     }
 
     @NonNull
@@ -162,30 +77,14 @@ public class SearchFriendPagerAdapter extends PagerAdapter {
         }
         this.isLoading = true;
         Log.d("Load", "new item");
-        String jsonText= "";
-        try {
-            //đọc và chuyển về JSONObject
-            jsonText = readText(context, R.raw.usertest);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        JSONObject userObj = new JSONObject();
-        try {
-            //đọc và chuyển về JSONObject
-            userObj = new JSONObject(jsonText);
-        } catch (JSONException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        for (int i = 0; i < userObj.length(); i++) {
-            if (userObj.has("user"+i)){
-                try {
-                    this.dataBuff.add(userObj.getJSONObject("user"+i).getString("name"));
-                } catch(JSONException e) {
-                    e.printStackTrace();
-                }
-            }
+
+        // load new data from server
+        Random r = new Random();
+        String item = "";
+        for (int i = 0; i < 20; i++) {
+            item = "item" + r.nextInt();
+            this.dataBuff.add(item);
+            Log.d("item", "new item");
         }
 
         // update isLoading = false when load data success
