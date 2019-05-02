b784a3c6c06d621f7f0bddfe432dbe00970354a3
Quang Võ <vnhquang2011@gmail.com>
2019-05-02 14:40:06 +0700

    Revert "Auto stash before merge of "search-friend-design" and "origin/master""
    This reverts commit dc85a56876663f8cf2c9458ca98f49bbcc652743.

--- app/src/main/java/com/example/tinder/search_friend/SearchFriendPagerAdapter.java
+++ app/src/main/java/com/example/tinder/search_friend/SearchFriendPagerAdapter.java
@@ -3,20 +3,23 @@ package com.example.tinder.search_friend;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.model.User;
 import com.example.tinder.R;
 
 import java.util.ArrayList;
+import java.util.Random;
+
 import androidx.navigation.Navigation;
 
 public class SearchFriendPagerAdapter extends PagerAdapter {
@@ -29,13 +32,12 @@ public class SearchFriendPagerAdapter extends PagerAdapter {
 
     public SearchFriendPagerAdapter(Context context) {
         this.context = context;
+        this.isLoading = false;
         dataBuff = new ArrayList<>();
         for (int i = 1; i < 30; i++) {
             dataBuff.add(new User(i,i + "@gmail.com", "fds", "user " + i, i, "male"));
         }
     }
-
-    //
 
     @NonNull
     @Override
@@ -62,7 +64,7 @@ public class SearchFriendPagerAdapter extends PagerAdapter {
     }
 
     private boolean isExhaustedBuff() {
-        return this.dataBuff.size() < 3;
+        return this.dataBuff.size() < 20;
     }
 
     public boolean isBufferEmpty() {
@@ -70,6 +72,10 @@ public class SearchFriendPagerAdapter extends PagerAdapter {
     }
 
     private void loadData() {
+        if (this.isLoading) {
+            return;
+        }
+        this.isLoading = true;
         Log.d("Load", "new item");
 
         // load new data from server
@@ -80,9 +86,8 @@ public class SearchFriendPagerAdapter extends PagerAdapter {
             Log.d("item", "new item");
         }
 
-        String jsonFriendURL = "https://api.myjson.com/bins/r9pkg";
-        SearchFriendFragment.DownloadJsonTask jsontask = new SearchFriendFragment.DownloadJsonTask(dataBuff);
-        jsontask.execute(jsonFriendURL);
+        // update isLoading = false when load data success
+        this.isLoading = false;
     }
 
     @Override
